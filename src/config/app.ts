df68b1c8b1a0d6bb1f416e42db21f32e3b87da19
Keenan MARTIN <kesmartin2004@gmail.com>
2025-03-25 09:46:34 +0100

    Adding security to middleware : use of helmet and cors

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response }  from "express";
+import helmet from 'helmet'
 import userRoutes from "../routes/user.routes";
+import cors from 'cors';
 // inference de type : lorsque le language devine le type d'une variable a partir du type de données qui sert a l'initialisation
 export const app: Application = express();
 import errorHandler from '../middlewares/error.middleware';
@@ -8,6 +10,10 @@ import swaggerDocument from '../docs/swagger.json';
 
 
 // Middlewares
+// Sécurité avec helmet
+app.use(helmet())
+//Partage des ressources (cors)
+app.use(cors())
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -19,4 +25,6 @@ app.get('/', (req: Request, res: Response) => {
     res.status(200).json('Bienvenue sur mon API Express avec TypeScript ✨' );
 
 });
+
+
 
--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI as string);
-    console.log('✅ MongoDB connecté');
+    console.log('Connecté à MongoDB avec succès !');
   } catch (err) {
     console.error('Erreur de connexion MongoDB', err);
     process.exit(1);
